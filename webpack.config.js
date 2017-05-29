acb9c2ecdaad4b8f6e54ab34a83670e4792f317c
Samtfm <sfmemail@gmail.com>
2017-05-29 11:23:11 -0700

    install webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+  context: __dirname,
+  entry: "./entry.js",
+  output: {
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ["es2015","react"]
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
